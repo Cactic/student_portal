2ed3ec49b21a5fa6827996d144996b552cd440b2
Dion Behre <dionjbehre@hotmail.com>
2018-10-12 11:07:53 +0200

    Scene switching

--- app/src/main/java/com/example/prive/student_portal/activity_add_url.java
+++ app/src/main/java/com/example/prive/student_portal/activity_add_url.java
@@ -5,23 +5,22 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class activity_add_url extends AppCompatActivity {
 
     FloatingActionButton button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_add_url);
 
-        button = findViewById(R.id.addButton);
+        button = findViewById(R.id.urlButton);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,activity_add_url.class);
+                Intent intent = new Intent(activity_add_url.this,MainActivity.class);
                 startActivity(intent);
             }
         });
